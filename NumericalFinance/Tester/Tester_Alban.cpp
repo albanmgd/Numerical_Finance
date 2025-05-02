107506a1996b03ee71f3308e4b4f6975f1433fd2
albanmgd <magerandalban@gmail.com>
2025-05-02 06:10:37 +0200

    fixing indexing - still invertibility issues

--- NumericalFinance/Tester/Tester_Alban.cpp
+++ NumericalFinance/Tester/Tester_Alban.cpp
@@ -82,9 +82,9 @@ void TestLongstaffSchwarz(){
     /* Initial params to define our option */
     int dim = 3;
     double T = 1.; // Maturity
-    double K = 65;
+    double K = 60;
     size_t nbSteps = 365;
-    size_t nbSims = 1e4;
+    size_t nbSims = 1e3;
     std::vector<double> Spots = {100, 50, 60};
     std::vector<double> Vols = {0.10, 0.25, 0.16};
     double Rate = 0.05;
@@ -94,14 +94,14 @@ void TestLongstaffSchwarz(){
         TestCorrelMatrix[i][i] = 1.0;
     }
 
-    /* Params defining our pricing */
+    /* Params defining our random nb generators */
     UniformGenerator* Unif = new EcuyerCombined();
     NormalBoxMuller* NormBox = new NormalBoxMuller(0., 1., Unif);
     BSEulerND TestScheme = BSEulerND(NormBox, dim, Spots, Rate, Vols, &TestCorrelMatrix);
     int L = 3; /* Param controlling the expansion order for the basis functions */
 
     /* Begin by generating directly nbSim asset paths for the d assets */
-    std::vector<std::vector<std::unique_ptr<SinglePath>>> AllAssetPaths(nbSims);
+    vector<vector<std::unique_ptr<SinglePath>>> AllAssetPaths(nbSims);
     for (size_t nSimul = 0; nSimul < nbSims; nSimul++) {
         AllAssetPaths[nSimul].resize(dim); /* resizing to hold d SinglePath */
         TestScheme.Simulate(0, T, nbSteps, false); /* Simulating the paths - enables us to reuse antithetic control variate*/
@@ -110,28 +110,30 @@ void TestLongstaffSchwarz(){
         }
     }
     // Backward induction for the stopping times
-    std::vector<std::vector<double>> stoppingTimes(nbSims, std::vector<double>(dim, T)); /* initialization */
+    std::vector<std::vector<double>> stoppingTimes(nbSims, std::vector<double>(nbSteps, T)); /* initialization */
+//    std::vector<SinglePath*> stoppingTimes;
 
     // Keeping in memory the baskets paths to avoid recomputing them too
-    std::vector<std::vector<double>> basketValues(nbSteps, std::vector<double>(nbSims, 0.0));
-    // Now for each timestep we need to get the weights
+    vector<vector<double>> basketValues(nbSteps, vector<double>(nbSims, 0.0));
+    // Variables used later have to be declared here
     double delta_t = T / nbSteps;
-    std::vector<std::vector<double>> basisVectors(nbSims, std::vector<double>(L, 0.0)); // used to construct phi
-
+    /*std::vector<std::vector<double>> basisVectors(nbSims, std::vector<double>(L, 0.0));*/ // used to construct phi
+    // Looping through time, starting at T
     for (int i=(nbSteps - 1); i >= 0; i--){
+        std::vector<std::vector<double>> basisVectors(nbSims, std::vector<double>(L, 0.0));
         double currentTimestep = i * delta_t; double previousTimestep = (i + 1) * delta_t; /* going backward */
         // Initializing the right hand part of the sum to optimize
         std::vector<double> basisDecompositionVector (L, 0.0);
-
+        // Going through all the simulations for one time-step
         for (size_t nSimul = 0; nSimul < nbSims; nSimul++) {
+            // Computing the basket value for that simulation at the above timestep
             for (size_t j=0; j < dim; j++){
                 basketValues[i][nSimul] += Weights[j] * AllAssetPaths[nSimul][j]->GetValue(currentTimestep);
             }
             double basketValue = basketValues[i][nSimul];
-            double previousStoppingTime = stoppingTimes[nSimul][i];
-            double mulFactor = exp(- Rate * (previousStoppingTime - currentTimestep)) *  std::max<double>(basketValue - K, 0);;
+            double previousStoppingTime = stoppingTimes[nSimul][i + 1];
+            double mulFactor = exp(- Rate * (previousStoppingTime - currentTimestep)) *  std::max<double>(basketValue - K, 0);
             /* Constructing the P vector */
-
             for (size_t l=0; l < L; l++){
                 double basisScalar= pow(basketValue, l);
                 basisVectors[nSimul][l] = basisScalar;
@@ -141,23 +143,27 @@ void TestLongstaffSchwarz(){
         // Can now construct the Phi Matrix
         Matrix Phi = Matrix(basisVectors);
         Matrix H = Matrix(Phi.getTranspose() * Phi);
-        // Some gymnastic with matrixes to build a column matrix
+        // Some gymnastic with matrices to build a column matrix - L usually not too big so should be OK
         std::vector<std::vector<double>> columnMatrix(L, std::vector<double>(1));
-        for (size_t i = 0; i < L; ++i) {
-            columnMatrix[i][0] = basisDecompositionVector[i];
+        for (size_t l = 0; l < L; l++) {
+            columnMatrix[l][0] = basisDecompositionVector[l];
         }
         Matrix basisColumnMatrix(columnMatrix);
         // Can now compute the weights vector alpha
         Matrix Alpha = H.inverseLU() * basisColumnMatrix;
-        // And finally update the stopping times - need to reloop through all the paths
+        // And finally update the stopping times - need to re-loop through all the paths
         for (size_t nSimul = 0; nSimul < nbSims; nSimul++) {
-            // Computing the basis expansion with alpha coeffs
+            // Computing the basis expansion with alpha coefficients
             double alphaBasisExpansion = 0.0; double payoffSimulation = std::max<double>(basketValues[i][nSimul] - K, 0);
             for (size_t l=0; l < L; l++){
                 alphaBasisExpansion += Alpha.data[l][0] * basisVectors[nSimul][l];
             }
-            if (payoffSimulation >= alphaBasisExpansion) stoppingTimes[nSimul][i] = currentTimestep;
-            else stoppingTimes[nSimul][i] = stoppingTimes[nSimul][i+1];
+            if (payoffSimulation >= alphaBasisExpansion){
+                stoppingTimes[nSimul][i] = currentTimestep;
+            }
+            else {
+                stoppingTimes[nSimul][i] = stoppingTimes[nSimul][i+1];
+            }
         }
     }
     // Finally we can value the option
@@ -165,7 +171,6 @@ void TestLongstaffSchwarz(){
     for (size_t nSimul = 0; nSimul < nbSims; nSimul++) {
         double stoppingTimeSimul = stoppingTimes[nSimul][0];
         int indexStoppingTimeSimul = distance(stoppingTimes[nSimul].begin(),find(stoppingTimes[nSimul].begin(), stoppingTimes[nSimul].end(), stoppingTimeSimul));
-
         payoff += exp(- Rate * stoppingTimeSimul) * std::max<double>(basketValues[indexStoppingTimeSimul][nSimul] - K, 0);
     }
     cout << "The price of the Bermudean Basket Call is : " << payoff / nbSims << endl;
